c6ada56999eae778a13cf1b37f71c1b4b1448cf5
zorxx <zorxx@zorxx.com>
2016-02-06 13:23:38 -0600

    Add support for AVR

--- examples/common/blink/blink.cpp
+++ examples/common/blink/blink.cpp
@@ -13,18 +13,19 @@
   by Scott Fitzgerald
  */
 
+#define GPIO_PIN 13
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  pinMode(GPIO_PIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+  digitalWrite(GPIO_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);              // wait for a second
+  digitalWrite(GPIO_PIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);              // wait for a second
 }
 
