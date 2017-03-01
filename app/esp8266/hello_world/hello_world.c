15b727c8d0190376718c9c720117117dd1b145a5
zorxx <zorxx@zorxx.com>
2017-02-28 23:25:11 -0600

    Miscellaneous fixes

--- app/esp8266/hello_world/hello_world.c
+++ app/esp8266/hello_world/hello_world.c
@@ -13,19 +13,17 @@ LOCAL uint32_t counter = 0;
 
 LOCAL void ICACHE_FLASH_ATTR hello_cb(void *arg)
 {
-	ets_uart_printf("Hello World %lu!\r\n", counter);
-        ++counter;
+    ets_uart_printf("Hello World %lu!\r\n", counter);
+    ++counter;
 }
 
 void user_init(void)
 {
-	// Configure the UART
-	uart_init(BIT_RATE_115200, BIT_RATE_115200);
-	// Set up a timer to send the message
-	// os_timer_disarm(ETSTimer *ptimer)
-	os_timer_disarm(&hello_timer);
-	// os_timer_setfn(ETSTimer *ptimer, ETSTimerFunc *pfunction, void *parg)
-	os_timer_setfn(&hello_timer, (os_timer_func_t *)hello_cb, (void *)0);
-	// void os_timer_arm(ETSTimer *ptimer,uint32_t milliseconds, bool repeat_flag)
-	os_timer_arm(&hello_timer, DELAY, 1);
+    // Configure the UART
+    uart_div_modify(0, UART_CLK_FREQ / 115200);
+
+    // Set up a timer to send the message
+    os_timer_disarm(&hello_timer);
+    os_timer_setfn(&hello_timer, (os_timer_func_t *)hello_cb, (void *)0);
+    os_timer_arm(&hello_timer, DELAY, 1);
 }
